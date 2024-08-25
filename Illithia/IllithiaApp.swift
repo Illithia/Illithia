63e0836b6bde5eba8d22d04c5edc572152dc7338
angelocarasig <angelocarasig15@gmail.com>
2024-08-26 01:01:19 +1000

    Initial Commit

---
+++ Illithia/IllithiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IllithiaApp.swift
+//  Illithia
+//
+//  Created by Angelo Carasig on 26/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct IllithiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
